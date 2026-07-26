b24261918d8bbe9291abda40dce5ca14bd41b245
moonpasha98 <moonpasha38@gmail.com>
2026-07-26 22:52:08 +0530

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', background: '#222', color: '#fff' }}>
+      <h2>Navbar</h2>
+    </nav>
+  );
+}
