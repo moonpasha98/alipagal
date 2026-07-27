013f3a93ad96f7d9d63e2eff8a118400518f251f
moonpasha98 <moonpasha38@gmail.com>
2026-07-27 18:22:39 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import Navbar from './components/Navbar';
 import Hero from './components/hero';
 import Gallery from './components/Gallery';
 import Testimonials from './components/Testimonials';
-
+import Services from './components/Services';
 export default function App() {
   return (
     <div className="bg-[#FAF8F5] text-[#1F1F1F] min-h-screen font-poppins">
       <Navbar onOpenBooking={() => alert('Booking system updating soon!')} />
       <Hero onOpenBooking={() => alert('Booking system updating soon!')} />
+      <Services />
       <Gallery />
       <Testimonials />
     </div>
