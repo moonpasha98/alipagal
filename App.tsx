cb2616ec1b14aaf0b1a9dedef274a0f42139ea7b
moonpasha98 <moonpasha38@gmail.com>
2026-07-27 15:54:15 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,36 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/hero';
-import Services from './components/Services';
 import Gallery from './components/Gallery';
-import BookingModal from './components/BookingModal';
-import Footer from './components/Footer';
 
 export default function App() {
-  const [isBookingModalOpen, setBookingModalOpen] = useState(false);
-  const [preselectedService, setPreselectedService] = useState<string>('HD Bridal Makeup');
-
-  const handleOpenBooking = (serviceName?: string) => {
-    if (serviceName) {
-      setPreselectedService(serviceName);
-    } else {
-      setPreselectedService('HD Bridal Makeup');
-    }
-    setBookingModalOpen(true);
-  };
-
   return (
     <div className="bg-[#FAF8F5] text-[#1F1F1F] min-h-screen font-poppins">
-      <Navbar onOpenBooking={() => handleOpenBooking()} />
-      <Hero onOpenBooking={() => handleOpenBooking()} />
-      <Services onOpenBooking={(service) => handleOpenBooking(service)} />
+      <Navbar onOpenBooking={() => alert('Booking system updating soon!')} />
+      <Hero onOpenBooking={() => alert('Booking system updating soon!')} />
       <Gallery />
-      <Footer />
-      <BookingModal
-        isOpen={isBookingModalOpen}
-        onClose={() => setBookingModalOpen(false)}
-        selectedService={preselectedService}
-      />
     </div>
   );
 }
