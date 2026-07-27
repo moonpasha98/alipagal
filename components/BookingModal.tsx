0b26bf983510a0d6f00a72d011c746c88aa32c1b
moonpasha98 <moonpasha38@gmail.com>
2026-07-27 18:42:57 +0530

    Update BookingModal.tsx

--- components/BookingModal.tsx
+++ components/BookingModal.tsx
@@ -30,6 +30,7 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, onClose }) => {
         <button 
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-400 hover:text-gray-700 text-2xl font-bold"
+          type="button"
         >
           &times;
         </button>
